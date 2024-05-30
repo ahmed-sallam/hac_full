ae1103abffcbc5b385a3696b63d2c2cd5aec2c10
ahmed-sallam <devahmedsallam@gmail.com>
2024-05-30 12:43:02 +0300

    Init RFPQ in (Frontend)

---
+++ frontend/src/app/dashboard/purchases/rfpq/rfpq.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rfpq',
+  standalone: true,
+  imports: [],
+  templateUrl: './rfpq.component.html',
+  styles: ``
+})
+export class RfpqComponent {
+
+}
