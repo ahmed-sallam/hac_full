7f058af784fbc1489e348cede5fde1eab2ee7a05
ahmed-sallam <devahmedsallam@gmail.com>
2024-07-07 10:38:50 +0300

    Update Create Bid Summary functions & Adding quotations modal (FrontEnd)

---
+++ frontend/src/app/dashboard/purchases/bid-summary/quotations-modal/quotations-modal.component.ts
@@ -0,0 +1,48 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {ReactiveFormsModule} from "@angular/forms";
+import {TranslatePipe} from "../../../../pipes/translate.pipe";
+import {DatePipe} from "@angular/common";
+
+@Component({
+  selector: 'app-quotations-modal',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    TranslatePipe,
+    DatePipe
+  ],
+  templateUrl: './quotations-modal.component.html',
+  styles: ``
+})
+export class QuotationsModalComponent {
+  @Input() language!: string;
+
+
+  tableColumns = [
+      'id',
+      'date',
+      'product',
+      'netPrice',
+      'sarPrice',
+      'isLocal',
+      'paymentTerms',
+        'delivery'
+  ]
+    @Input() quotations!: any;
+    @Input() supplier!: any;
+    @Output() closeModal = new EventEmitter<void>();
+    @Output() selectQuotation = new EventEmitter<any>();
+
+   onClose(){
+       this.closeModal.emit();
+   }
+
+    OnSelectItem(item: any) {
+        this.selectQuotation.emit(item);
+    }
+
+    selectItemClose(item: any) {
+       this.OnSelectItem(item);
+        this.onClose();
+    }
+}
