e43d73bffd9590b5b8d5d1f642ea39663d39a5b6
ahmed-sallam <devahmedsallam@gmail.com>
2024-10-30 12:58:17 +0300

    missed items  (FrontEnd)

---
+++ frontend/src/app/dashboard/sales/orders/one-order/one-order.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-one-order',
+  standalone: true,
+  imports: [],
+  templateUrl: './one-order.component.html',
+  styles: ``
+})
+export class OneOrderComponent {
+
+}
---
+++ frontend/src/app/dashboard/sales/orders/orders.service.ts
@@ -0,0 +1,67 @@
+import {AppService} from "../../../app.service";
+import {HttpClient} from "@angular/common/http";
+import {Injectable} from "@angular/core";
+import {Observable} from "rxjs";
+import {SaleOrdersResponse} from "./interfaces/SaleOrdersResponse";
+import {CreateSaleOrder} from "./interfaces/CreateSaleOrder";
+import {OneSaleOrder} from "./interfaces/OneSaleOrder";
+
+@Injectable({
+    providedIn: "root",
+})
+export class OrdersService {
+
+    constructor(private http: HttpClient, private appService: AppService) {
+    }
+
+    getOrders(
+        page: number,
+        sort?: string,
+        size?: number,
+        search?: string,
+        ref?: number,
+        user?: number,
+        status?: string,
+        customer?: number,
+        date?: string,
+    ): Observable<SaleOrdersResponse> {
+        let link: string = `${this.appService.baseApi}/sale-orders?page=${page}`;
+        if (sort) {
+            link += `&sort=${sort}`;
+        }
+        if (ref) {
+            link += `&ref=${ref}`;
+        }
+
+        if (user) {
+            link += `&user=${user}`;
+        }
+
+        if (status) {
+            link += `&status=${status}`;
+        }
+        if (size) {
+            link += `&size=${size}`;
+        }
+        if (search) {
+            link += `&order=${search}`;
+        }
+        if (date) {
+            link += `&date=${date}`;
+        }
+        if (customer) {
+            link += `&customer=${customer}`;
+        }
+        return this.http.get<SaleOrdersResponse>(link);
+    }
+
+    createSaleOrder(saleOrder: CreateSaleOrder): Observable<any> {
+        let link: string = `${this.appService.baseApi}/sale-orders`;
+        return this.http.post<any>(link, saleOrder);
+    }
+
+    getOneSaleOrder(id: number): Observable<OneSaleOrder> {
+        let link: string = `${this.appService.baseApi}/sale-orders/${id}`;
+        return this.http.get<OneSaleOrder>(link);
+    }
+}
